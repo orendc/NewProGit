c5092463781219a48384483a80a8b3693508d6c6
orendc <93371228+orendc@users.noreply.github.com>
2021-11-01 10:17:58 +0530

    new syntax

--- NewProGit/Program.cs
+++ NewProGit/Program.cs
@@ -11,7 +11,7 @@ namespace NewProGit
             Console.WriteLine("From Vs");
             Console.WriteLine("New git");
             Console.WriteLine("From VS1");
-            Console.WriteLine("From Github2);
+            Console.WriteLine("From Github2");
             Console.WriteLine("From VS2");
             Console.ReadLine();
         }
