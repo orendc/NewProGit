32fabe885ce1ea5cb4b1ff8d5e42c11d151dff7c
orendc <93371228+orendc@users.noreply.github.com>
2021-11-01 09:45:34 +0530

    change in github

--- NewProGit/Program.cs
+++ NewProGit/Program.cs
@@ -7,6 +7,7 @@ namespace NewProGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("From GitHub");
             Console.ReadLine();
         }
     }
