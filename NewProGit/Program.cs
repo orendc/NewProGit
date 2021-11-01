4e56fa05afebf0b6796109e3fa7d98c4d9411936
orendc <orendcunha2699@gmail.com>
2021-11-01 10:06:03 +0530

    new

--- NewProGit/Program.cs
+++ NewProGit/Program.cs
@@ -9,6 +9,7 @@ namespace NewProGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("From GitHub");
             Console.WriteLine("From Vs");
+            Console.WriteLine("From VS1");
             Console.ReadLine();
         }
     }
