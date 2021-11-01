aea831ce3283d668307911a1903609fcd7c06e57
orendc <orendcunha2699@gmail.com>
2021-11-01 09:47:45 +0530

    Change in vs

--- NewProGit/Program.cs
+++ NewProGit/Program.cs
@@ -7,6 +7,7 @@ namespace NewProGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("From Vs");
             Console.ReadLine();
         }
     }
