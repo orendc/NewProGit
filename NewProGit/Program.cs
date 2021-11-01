5b715c735e33b910d0762f8e0705efd9918ae7cb
orendc <orendcunha2699@gmail.com>
2021-11-01 09:35:34 +0530

    Add project files.

---
+++ NewProGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewProGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
